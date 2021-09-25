c8cbb0496bba99a6ac5176846767da21ed94edb6
Kasra Ghavidel <32060084+kasraghavidel@users.noreply.github.com>
2021-09-25 16:13:32 +0330

    addind accordion

--- js/accordion.js
+++ js/accordion.js
@@ -1,21 +1,40 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let comAaccordion_div = document.querySelectorAll(".accordion");
-    comAaccordion_div.forEach(comAaccordion_div => {
-      let comHeader_div = comAaccordion_div.querySelectorAll(".accordion__header");
-      let comBody_div = comAaccordion_div.querySelectorAll(".accordion__body");
-      let comItem_li = comAaccordion_div.querySelectorAll(".accordion__item");
-      comHeader_div.forEach(comHeader_div => {
-        comHeader_div.addEventListener('click', function () {
-              if (comHeader_div.nextElementSibling.classList.contains('accordion__body--active'))	{
-                  this.nextElementSibling.classList.remove('accordion__body--active');
-              } else{
-                  comBody_div.forEach(comBody_div => {
-                  comBody_div.classList.remove('accordion__body--active');
-                      });
-                  this.nextElementSibling.classList.add('accordion__body--active');
-                  }
-        });
-      });
+// JavaScript Document
+
+  let accordion = document.querySelectorAll(".accordion");
+  accordion.forEach(thisAccordion => {
+
+    let accordion_header = thisAccordion.querySelectorAll(".accordion_header");
+    let accordion_body = thisAccordion.querySelectorAll(".accordion_body");
+    let accordion_item = thisAccordion.querySelectorAll(".accordion_item");
+		let accordion_lineII = thisAccordion.getElementsByClassName("accordion_lineII");
+		
+    accordion_header.forEach(thisAccordion_header => {
+			let accordion_icon = document.createElement("DIV"); 
+			accordion_icon.classList.add('accordion_icon');
+			let accordion_lineI = document.createElement("DIV"); 
+			accordion_lineI.classList.add('accordion_lineI');
+			let accordion_lineII = document.createElement("DIV");
+			accordion_lineII.classList.add('accordion_lineII')
+			accordion_icon.appendChild(accordion_lineI);
+			accordion_icon.appendChild(accordion_lineII);
+			thisAccordion_header.appendChild(accordion_icon);
     });
+    accordion_header.forEach(thisAccordion_header => {
+      thisAccordion_header.addEventListener('click', function () {
+			if (thisAccordion_header.nextElementSibling.classList.contains('accordion_item--active'))	{
+				this.nextElementSibling.classList.remove('accordion_item--active');
+				this.lastChild.lastChild.classList.remove('accordion_lineII--open');
+			} else{
+				for(let i = 0; i < accordion_lineII.length; i++ ){
+					accordion_lineII[i].classList.remove('accordion_lineII--open');
+				}
+				accordion_body.forEach(thisAccordion_body => {
+          thisAccordion_body.classList.remove('accordion_item--active');
+					});
+				this.nextElementSibling.classList.add('accordion_item--active');
+				this.lastChild.lastChild.classList.add('accordion_lineII--open');
+				}
+      });
   });
-  
+  });
+
