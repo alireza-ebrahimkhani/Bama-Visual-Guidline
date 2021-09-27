94e63fcf1dfe51da8d9e23379e5748c07640cf20
taran3395 <46003621+taran3395@users.noreply.github.com>
2021-09-27 23:31:44 +0330

    toolbar-index-scss

--- js/toolBar.js
+++ js/toolBar.js
@@ -1,33 +1,17 @@
-let index = document.querySelector('.index');
-let toolBar_wrapper = document.querySelector('.toolBar_wrapper');   
-let sideBar = document.querySelector('.sideBar');  
-let index_dynamicTitle = document.querySelector('.index_dynamicTitle');
-let index_nav = document.querySelector('.index_nav');
+let index = document.querySelector(".index");
+let toolBar_wrapper = document.querySelector(".toolBar_wrapper");
+let sideBar = document.querySelector(".sideBar");
+let index_dynamicTitle = document.querySelector(".index_dynamicTitle");
+let index_nav = document.querySelector(".index_nav");
 
-index_dynamicTitle.addEventListener('click' , ()=>{
-    index_nav.classList.toggle('index_nav--open');
-    index_dynamicTitle.classList.toggle('index_dynamicTitle--open');
-})
-
-if( window.outerWidth <= 1024){
-    toolBar_wrapper.appendChild(index);
-
-}else{
-    sideBar.appendChild(index);
-}
-
-
-window.addEventListener("resize" , function(){ 
-    if( window.outerWidth <= 1024){
-        toolBar_wrapper.appendChild(index);
-    }else{
-        sideBar.appendChild(index);
-    }
+index_dynamicTitle.addEventListener("click", () => {
+  index_nav.classList.toggle("index_nav--open");
+  index_dynamicTitle.classList.toggle("index_dynamicTitle--open");
 });
 
-window.addEventListener('scroll', ()=>{
-    let active_link = document.querySelector('.is-active-link');
-    index_nav.classList.remove('index_nav--open');
-    index_dynamicTitle.classList.remove('index_dynamicTitle--open');
-    index_dynamicTitle.innerHTML=active_link.innerHTML;
-})
+window.addEventListener("scroll", () => {
+  let active_link = document.querySelector(".is-active-link");
+  index_nav.classList.remove("index_nav--open");
+  index_dynamicTitle.classList.remove("index_dynamicTitle--open");
+  index_dynamicTitle.innerHTML = active_link.innerHTML;
+});
