6515799cecd28ff8ccb1ec6feb16424c0e655d4c
Kasra Ghavidel <32060084+kasraghavidel@users.noreply.github.com>
2021-10-03 17:33:24 +0330

    ش

--- js/toolBar.js
+++ js/toolBar.js
@@ -3,15 +3,27 @@ let toolBar_wrapper = document.querySelector(".toolBar_wrapper");
 let sideBar = document.querySelector(".sideBar");
 let index_dynamicTitle = document.querySelector(".index_dynamicTitle");
 let index_nav = document.querySelector(".index_nav");
-
+let index_is_open = false;
 index_dynamicTitle.addEventListener("click", () => {
   index_nav.classList.toggle("index_nav--open");
   index_dynamicTitle.classList.toggle("index_dynamicTitle--open");
+  index_is_open = true;
 });
 
-window.addEventListener("scroll", () => {
+$(window).on('DOMMouseScroll mousewheel', _.throttle( closeIndex, 1000, {trailing: false}));  
+function closeIndex(){
+  
+  if (index_is_open = true){
+    index_nav.classList.remove("index_nav--open");
+    index_dynamicTitle.classList.remove("index_dynamicTitle--open");
+    index_is_open = false;
+  }
+}
+$(window).on('DOMMouseScroll mousewheel', _.throttle( changeTitle, 1000, {trailing: false}));  
+function changeTitle(){
   let active_link = document.querySelector(".is-active-link");
   index_nav.classList.remove("index_nav--open");
   index_dynamicTitle.classList.remove("index_dynamicTitle--open");
   index_dynamicTitle.innerHTML = active_link.innerHTML;
-});
+}
+
